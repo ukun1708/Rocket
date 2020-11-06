23d4c9587ad2bf3fe9dd3ce1a3fe2b4f758f88ac
Виталий Лукин <mr.lukin1990@mail.ru>
2020-11-07 00:33:13 +0300

    Update project
    Добавил индикатор топлива, произвел запекание света

---
+++ Assets/Scripts/BirningFuel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirningFuel : MonoBehaviour
+{
+    public float birnFuel = 10;
+
+    LevelHealth lvlH;
+
+    StartRocket sR;
+
+    Rigidbody rB;
+
+    void Start()
+    {
+        lvlH = GetComponent<LevelHealth>();
+        sR = GetComponent<StartRocket>();
+        rB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StartRocket.Singletone.isPressed == true)
+        {
+            lvlH.lvlHealth -= (birnFuel) * Time.deltaTime;
+        }
+
+        if (lvlH.lvlHealth <= 0f)
+        {
+            sR.enabled = false;
+
+            rB.useGravity = true;
+
+            StartRocket.Singletone.isPressed = false;
+        }
+    }
+}
---
+++ Assets/Scripts/LevelHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelHealth : MonoBehaviour
+{
+    public float lvlHealth = 100;
+
+    public Slider mySlyder;
+
+    void Update()
+    {
+        mySlyder.value = lvlHealth;
+    }
+}
